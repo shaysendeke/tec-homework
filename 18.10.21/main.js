68272bf927e0b3130cd150d8de0506d277ed9b4a
shaysendeke <77267283+shaysendeke@users.noreply.github.com>
2021-10-18 23:31:02 +0300

    home wotk 18/10/21

---
+++ 18.10.21/main.js
@@ -0,0 +1,54 @@
+// !express_1
+// console.log("app is loading");
+// const express = require("express");
+// const app = express();
+// const path = require ("path")
+// const PORT = 8080;
+// console.log(__dirname);
+
+// app.get("/", (req, res) => {
+//     res.sendFile(path.join(__dirname, "public", "index.html"));
+//   });
+//   app.listen(PORT);
+
+// !web_server_1.5
+// console.log("app is loading");
+// const express = require("express");
+// const app = express();
+// const path = require ("path")
+// const PORT = 8080
+// // console.log(__dirname);
+
+// app.get("/",(req, res) => {
+//     res.sendFile(path.join(__dirname, "public", "index.html" ))
+// })
+
+// app.get("/about", (req, res) => {
+//     res.sendFile(path.join(__dirname, "public", "about.html"))
+// })
+// app.listen(PORT)
+
+// !web_server_1.7
+// console.log("app is loading");
+// const express = require("express");
+// const app = express();
+// const path = require("path")
+// const PORT= 8080
+
+// app.get("/", (req, res) => {
+//     res.sendFile(path.join(__dirname, "public", "index.html" ))
+// })
+// app.get("/events", (req, res ) => {
+//     res.sendFile(path.join (__dirname,"public", "events.html" ))
+// })
+// app.get("/courses", (req, res) =>{
+//     res.sendFile(path.join(__dirname, "public", "courses.html"))
+// })
+// app.get("/testimonial", (req, res) => {
+//     res.sendFile(path.join(__dirname, "public", "tests.html"))
+// })
+// app.get("/contact", (req, res)=>{
+//     res.sendFile(path.join(__dirname, "public", "contact.html"))
+// })
+
+// app.listen(PORT)
