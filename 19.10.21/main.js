db351ba1c4f61ce05fa861c015ec905191d9b1b1
shaysendeke <77267283+shaysendeke@users.noreply.github.com>
2021-10-25 10:06:48 +0300

    homework

---
+++ 19.10.21/main.js
@@ -0,0 +1,30 @@
+// !express_pre_3_1
+// !המשתנה __dirname מייצג את תיקיית האם שבא נמצאים כל הקבצים שאיתם אנחנו רוצים לעבוד על ה nodejs & HTML.
+
+// const { appendFile } = require("fs");
+
+// !express_pre_3_2
+// !משמש כדי לציין  את המיקום של הקבצים שאנחנו רוצים להעביר לקובץ html
+
+// !express_pre_3_3
+// #app.js
+// #by the definition on publicpath
+// #change the directory name to public1, and also change the name in the path
+// #as mentioned above
+// #as mentioned above
+
+
+console.log("app is loading");
+
+// !weather location web server
+// console.log("app is loading");
+// const express = require("express");
+// const app = express();
+// const path = require ("path")
+// const PORT = 8080;
+// console.log(__dirname);
+
+// app.get("/", (req, res) => {
+//     res.sendFile(path.join(__dirname, "public", "index.html"));
+//   });
+//   app.listen(PORT);
---
+++ express_static_files/public/js/app.js
@@ -0,0 +1,1 @@
+console.log("check this on the browser console");
