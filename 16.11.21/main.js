d7c1ecefbd9be5625afc69dd79647b0d23ea6bbb
shaysendeke <77267283+shaysendeke@users.noreply.github.com>
2021-11-17 16:41:10 +0200

    16.11.21

---
+++ 16.11.21/main.js
@@ -0,0 +1,66 @@
+// !!js_valuetype_vs_referencetype_1
+// a=2
+// b=1
+// objA=7001
+// objB=7001
+
+// !!js_valuetype_vs_referencetype_2
+// b=1
+// a=3
+// objA=7001
+// objB=7001
+
+// !!js_valuetype_vs_referencetype_3
+// b=1
+// a=?
+
+// !!js_valuetype_vs_referencetype_4
+// array1 = [8, 18, 3, 6]
+// array2 = [8, 18, 3, 6]
+
+// !!js_valuetype_vs_referencetype_5
+// array1=[{ name: "Jim", age: 33 }, { name: "John", age: 55 }]
+// array2=[1, 2, 3, 6]
+
+// !!class_1_m
+// class Student {
+//     constructor (name, age, gender){
+//         this.name = name;
+//         this.age = age;
+//         this.gender = gender;
+//     }
+//     writeStudent(){
+//         console.log(this.name, this.age, this.gender);
+//         function isYoung (){
+//         if (18 < this.age) {
+//             console.log(true)
+//         }
+//      else {console.log(false);}
+//     }
+// }
+// }
+//     const student1 = new Student ("Aviv Avivi", 17, "Male");
+//     const student2 = new Student ("Daniel Danieli", 45, "Female");
+//     const student3 = new Student ("Israel Israel", 25, "Male")
+    // student1.writeStudent()
+    // student2.writeStudent()
+    // student3.writeStudent()
+
+    // let arrayOfStudents=[]
+    // arrayOfStudents.push(student3, student2, student1)
+    // console.log(arrayOfStudents);
+
+//     function isYoung (){
+//         for (let i=0; i<arrayOfStudents.length; i++){
+//         if (18 < arrayOfStudents[i].age) {
+//             console.log(true)
+//         }
+//      else {console.log(false);}
+//     }
+//     }
+// isYoung(arrayOfStudents)
+
+// isYoung(student1)
+
+
+
