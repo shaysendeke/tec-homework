c0d5ef22988de2ae9a80efdbe9ad8b2c721bef59
shaysendeke <77267283+shaysendeke@users.noreply.github.com>
2021-11-28 10:22:11 +0200

    addition to HW

---
+++ 25.11.21/hw_25.11.21/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Fetch from './components/fetch/Fetch';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      shay sendeke 25.11.21
+      </header>
+      < Fetch />
+    </div>
+  );
+}
+
+export default App;
