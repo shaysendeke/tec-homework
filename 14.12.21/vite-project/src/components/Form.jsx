276e6c2b8ce85b2fd096109d5235742b17022779
shaysendeke <77267283+shaysendeke@users.noreply.github.com>
2021-12-15 15:20:31 +0200

    additional 2

--- 14.12.21/vite-project/src/components/Form.jsx
+++ 14.12.21/vite-project/src/components/Form.jsx
@@ -1,39 +1,36 @@
-import React , {useState} from 'react';
+import React, { useState } from "react";
 
 const Form = () => {
-    const [Form, setForm] = useState({ name: "", email: "", password: "" });
+  const [Form, setForm] = useState({ name: "", email: "", password: "" });
 
-    const formDetails = e => {
-        e.preventDefault();
-        console.log(
-          `Name is: ${Form.name},
-           Password is : ${Form.password}, 
-           Email Adress Is : ${Form.email} `
-        );
-      };
-    
-    return (
-        <form >
-        <input
-          type="text"
-          value={Form.name}
-          placeholder="Name"
-          onChange={e => setForm({ ...Form, name: e.target.value})}
-        /><br/><br/>
-        <input
-          type="password"
-          value={Form.password}
-          placeholder="Password"
-          onChange={e => setForm({ ...Form, password: e.target.value })}
-        /><br/><br/>
-        <input
-        //   type="email"
-          value={Form.email}
-          placeholder="Email"
-          onChange={e => setForm({ ...Form, email: e.target.value })}
-        /><br/><br/>
-        <input type="submit" onClick={formDetails} />
-      </form>
+  const formDetails = (e) => {
+    e.preventDefault();
+    console.log(
+      `Name is: ${Form.name},
+       Password is : ${Form.password}, 
+       Email Adress Is : ${Form.email} `
     );
   };
+
+  return (
+    <form>
+      <input
+        placeholder="Name"
+        onChange={(e) => setForm({ ...Form, name: e.target.value })}
+      />
+      <br /><br />
+      <input
+        placeholder="Password"
+        onChange={(e) => setForm({ ...Form, password: e.target.value })}
+      />
+      <br /><br />
+      <input
+        placeholder="Email"
+        onChange={(e) => setForm({ ...Form, email: e.target.value })}
+      />
+      <br /><br />
+      <input type="submit" onClick={formDetails} />
+    </form>
+  );
+};
 export default Form;
