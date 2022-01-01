b076b7046d56af4cd5718f9bdb8fc8bb1631cf03
shaysendeke <77267283+shaysendeke@users.noreply.github.com>
2022-01-02 01:16:21 +0200

    homework

---
+++ 30.12.21/main.js
@@ -0,0 +1,32 @@
+// !1
+function sumOfDigit(n)
+{
+    if (n == 0)
+    return 0;
+    return (n % 10 + sumOfDigit(parseInt(n / 10)));
+}
+ 
+var num = 12345;
+var result = sumOfDigit(num);
+console.log( "Sum of digits in "+ num
+   +" is "+result );
+ 
+// sumOfDigit(1234)
+
+// !2
+
+function findMinimum(array, n)
+{
+     
+    if (n == 1)
+        return array[0];
+         
+    return Math.min(array[n - 1],
+        findMinimum(array, n - 1));
+}
+ 
+// Driver Code
+let array = [ 1, 4, 45, 6, -50, 10, 2 ];
+let n = array.length;
+ 
+document.write( findMinimum(array, n));
