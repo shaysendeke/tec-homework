042997472f2b024b8fc698a4cc23f05f8b18b5a6
shaysendeke <77267283+shaysendeke@users.noreply.github.com>
2022-01-04 09:48:09 +0200

    addition

--- 04.01.22/04.01.21/src/components/Login.jsx
+++ 04.01.22/04.01.21/src/components/Login.jsx
@@ -1,7 +1,48 @@
-import { useEffect, useState } from "react"
-const Login = () => {
-    
-}
+import { useState } from "react";
+import apiKey from './apiKey'
+import axios from "axios";
+import "./register.css"
 
+const Register = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorFromServer, setErrorFromServer] = useState("");
 
-export default Login
+  function register() {
+    const url = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${apiKey}`;
+    axios
+      .post(url, {
+        email,
+        password,
+      })
+      .then(function (response) {
+        console.log(response.data);
+      })
+      .catch(function (error) {
+        setErrorFromServer(error);
+        console.log(error);
+      });
+  }
+
+  return (
+    <form style={{textalign:"center"}}
+      onSubmit={(e) => {
+        e.preventDefault();
+        register();
+      }}
+    >
+      <label>Email | </label>
+      <input type="email" onChange={(e) => setEmail(e.target.value)} />
+      <br /><br/>
+      <label>Password | </label>
+      <input onChange={(e) => setPassword(e.target.value)} />
+      <br /><br/>
+      <input type="submit" value="Register" />
+      <p style={{ color: "red" }}>
+        {errorFromServer ? "error form server " : ""}
+      </p>
+    </form>
+  );
+};
+
+export default Register;
